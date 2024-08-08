bb8f51329bba7d5226a94809e2ccb88a969aece3
Evan <evan@ethicalhack.ing>
2024-08-07 23:30:39 -0400

    created nix development environment

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11") {} }:
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    (python3.withPackages (ps: [ ps.flask ]))
+    jq
+  ];
+}
